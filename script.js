a2af87a875c58128bd2f42313235c3aa9c05a111
MosoKris <mosomurmu@gmail.com>
2024-07-26 11:20:41 +0530

    Update portfolio sections and add horizontal scrolling feature

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+// script.js
 
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
 }
